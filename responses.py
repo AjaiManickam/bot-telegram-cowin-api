4caa15f1c54ed3259dc5a21e8aa67d608333f004
ajai.manickam <ajai.manickam@rubylife.com>
2021-05-29 23:07:19 +0530

    Minor change to check auto-deployment

--- responses.py
+++ responses.py
@@ -2,7 +2,7 @@ import Constants as keys
 
 def response_msg(input_text,update):
           user_message = input_text.lower()
-          msg_list = {'hi','hello','hey','yo','bro','dude'}
+          msg_list = {'hi','hello','hey','bro','dude'}
           if any(ele in user_message for ele in msg_list):
                     name = update.effective_user.first_name
                     return f'Hello {name}, {keys.STR_REPLY_TO_GREETINGS}'
