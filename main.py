73e73ec6a03b217017016e86226367eca6471163
ajai.manickam <ajai.manickam@rubylife.com>
2021-05-29 14:02:55 +0530

    Committing the first version of the code

---
+++ main.py
@@ -0,0 +1,84 @@
+from telegram.ext import *
+import methods as get
+import Constants as keys
+import responses as R
+
+print ('Initializing Bot')
+
+#Command and Message handlers
+
+def start_command(update, context):
+          print(f'{update.effective_user["username"]}: {update.message.text}')
+          print(f'Bot: {keys.STR_REPLY_TO_START_COMMAND}')
+          update.message.reply_text(f'Hey {update.effective_user.first_name}, {keys.STR_REPLY_TO_START_COMMAND}')
+
+def help_command(update, context):
+          print(f'Hey {update.effective_user["username"]}: {update.message.text}')
+          print('Bot: {Sending back help text}')
+          update.message.reply_text(f'{update.effective_user.first_name}, {keys.STR_REPLY_TO_HELP_COMMAND}')
+
+def vaccine_cbe_command(update, context):
+          print(f'{update.effective_user["username"]}: {update.message.text}')
+          vaccine_info = get.vaccine_info_as_text(539)
+          if len(vaccine_info) > 4096:
+                    print('Splitting the data and sending in smaller chunks since its too large...')
+                    final_note = ''
+                    max_typable_char = 4096 - len(final_note)
+                    for x in range(0, len(vaccine_info), max_typable_char):
+                              chunk_text = vaccine_info[x:x+max_typable_char]+final_note
+                              print(f'Bot: {chunk_text}')
+                              update.message.reply_text(chunk_text)
+          else:
+                    print(f'Bot: {vaccine_info}')
+                    update.message.reply_text(vaccine_info)
+          print(f'Data sent back successfully to {update.effective_user.username}')
+
+def vaccine_poy_command(update, context):
+          print(f'{update.effective_user["username"]}: {update.message.text}')
+          list_available_sessions = get.vaccine_availability_by_district(539)
+          is_slots_available_for_POY = False
+          final_text = ''
+          for session in list_available_sessions:
+                    if str(session['pincode'])[0:3] == "642" or str(session['pincode'])== '641202':
+                              is_slots_available_for_POY = True
+                              final_text = final_text + get.info_formatted(session)
+          if not is_slots_available_for_POY:
+                    final_text = keys.STR_SLOTS_NOT_AVAILABLE
+          print(f'Bot: {final_text}')
+          update.message.reply_text(final_text)
+          print(f'Data sent back successfully to {update.effective_user.username}')
+
+def handle_message(update, context):
+          print(f'{update.effective_user["username"]}: {update.message.text}')
+          text = str(update.message.text).lower()
+          response = R.sample_responses(text,update)
+          print(f'Bot: {response}')
+          update.message.reply_text(response)
+
+def error(update, context):
+          print(f'Update {update} caused error {context.error} - by {update.effective_user}')
+
+def main():
+          updater = Updater(keys.API_KEY, use_context=True)
+          dp = updater.dispatcher
+          #generic commands
+          dp.add_handler(CommandHandler("start",start_command))
+          dp.add_handler(CommandHandler("help",help_command))
+
+          #custom commands
+          dp.add_handler(CommandHandler("vaccine_cbe",vaccine_cbe_command))
+          dp.add_handler(CommandHandler("vaccine_poy",vaccine_poy_command))
+
+          #Message and Error Handling
+          dp.add_handler(MessageHandler(Filters.text,handle_message))
+          dp.add_error_handler(error)
+
+          print('Bot is started...')
+          updater.start_polling()
+          updater.idle()
+
+main()
+
+
+
+
---
+++ methods.py
@@ -0,0 +1,37 @@
+import requests
+import Constants as keys
+from datetime import datetime
+
+#Methods
+                          
+def vaccine_availability_by_district(district_id):
+
+          print(f'Getting data for the district ID - {district_id}...')
+          date = datetime.today().strftime('%d-%m-%Y')
+          url = f'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={district_id}&date={date}'
+          browser_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+          print(url)
+          response = requests.get(url, headers=browser_header)
+          print(response)
+          list_available_sessions = get_sessions_only_with_vaccine_available(response)
+          return list_available_sessions
+
+def get_sessions_only_with_vaccine_available(response):
+          list_sessions = response.json()["sessions"]
+          for i in range(len(list_sessions)-1,-1,-1):
+                    if list_sessions[i]["available_capacity"] <= 0: list_sessions.pop(i)
+          return list_sessions
+          
+def vaccine_info_as_text(district_id):
+
+          list_available_sessions = vaccine_availability_by_district(district_id)
+          final_text = ''
+          if len(list_available_sessions)==0:
+                    final_text = keys.STR_SLOTS_NOT_AVAILABLE
+          else:
+                    for session in list_available_sessions:
+                              final_text = final_text + info_formatted(session)
+          return final_text
+
+def info_formatted(session):
+         return "\nName: "+str(session['name']) +'\n'+ "Available Capacity: "+str(session['available_capacity']) +'\n' + "Min Age Limit: "+str(session['min_age_limit']) +'\n' + "Vaccine: "+str(session['vaccine'])+ '\n' + '----------------------------------------'
