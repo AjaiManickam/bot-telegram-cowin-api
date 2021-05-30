1f54017e1972cc7eddf9ac1c5cd0349bdd9829f2
ajai.manickam <ajai.manickam@rubylife.com>
2021-05-30 16:56:26 +0530

    Updated the reply message format for vaccine information

--- methods.py
+++ methods.py
@@ -33,7 +33,10 @@ def vaccine_info_as_text(district_id):
           return final_text
 
 def info_formatted(session):
-         return "\nName: "+str(session['name']) +'\n'+ "Available Capacity: "+str(session['available_capacity']) +'\n' + "Min Age Limit: "+str(session['min_age_limit']) +'\n' + "Vaccine: "+str(session['vaccine'])+ '\n' + '----------------------------------------'
+          rupee_symbol =  u"\u20B9"
+          fee = f"{rupee_symbol} {session['fee']}"
+          full_address = f"{session['name']}, {session['address']}, {session['block_name']} - {session['pincode']}"
+          return f"\nName: {full_address}\nTotal Available Capacity: {session['available_capacity']}\nDose 1 capacity: {session['available_capacity_dose1']}\nDose 2 capacity: {session['available_capacity_dose2']}\nAge Group: {session['min_age_limit']}+\nFee: {fee}\nVaccine: {session['vaccine']}\n\nRegister: https://selfregistration.cowin.gov.in/\n-------------------------------------------------------------"
 
 def print_input_info(update):
           print(f'{str(datetime.now())} {update.effective_user["username"]} (from {update.effective_chat["title"]} | {update.effective_chat["id"]}) :: {update.message.text}')
